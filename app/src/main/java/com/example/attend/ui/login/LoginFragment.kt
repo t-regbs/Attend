f02bb38502ce4cce7d89a7bcbe7c5579f5897872
Timilehin <aregbestimi@gmail.com>
2020-08-18 01:13:04 +0100

    updated login ui

--- app/src/main/java/com/example/attend/ui/login/LoginFragment.kt
+++ app/src/main/java/com/example/attend/ui/login/LoginFragment.kt
@@ -1,29 +1,25 @@
 package com.example.attend.ui.login
 
 import android.content.Intent
-import androidx.lifecycle.ViewModelProvider
-import androidx.fragment.app.Fragment
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Adapter
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.Toast
-import androidx.core.view.get
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
-import androidx.navigation.fragment.findNavController
 import com.example.attend.LecturerActivity
 import com.example.attend.MainActivity
-
 import com.example.attend.R
 import com.example.attend.databinding.FragmentLoginBinding
 import com.example.attend.utils.onTextChanged
-import kotlinx.android.synthetic.main.fragment_login.*
 import org.koin.androidx.viewmodel.ext.android.stateViewModel
 import timber.log.Timber
 
-class LoginFragment : Fragment(), AdapterView.OnItemSelectedListener {
+class LoginFragment : Fragment() {
 
     private val loginViewModel: LoginViewModel by stateViewModel(bundle = Bundle())
     private lateinit var binding: FragmentLoginBinding
@@ -34,7 +30,6 @@ class LoginFragment : Fragment(), AdapterView.OnItemSelectedListener {
             savedInstanceState: Bundle?
     ): View? {
         binding = FragmentLoginBinding.inflate(inflater, container, false)
-        binding.spinner.onItemSelectedListener = this
         return binding.root
     }
 
@@ -55,36 +50,30 @@ class LoginFragment : Fragment(), AdapterView.OnItemSelectedListener {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         setupObservers()
-        ArrayAdapter.createFromResource(
+        val type = arrayOf("Admin", "Lecturer")
+        val adapter = ArrayAdapter(
             requireContext(),
-            R.array.user_type_array,
-            android.R.layout.simple_spinner_item
-        ).also { adapter ->
-            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-            binding.spinner.adapter = adapter
-        }
+            R.layout.custom_simple_spinner_item,
+            type
+        )
+        binding.outlinedExposedDropdown.setAdapter(adapter)
 
         binding.username.editText?.setText(loginViewModel.username.value)
         binding.password.editText?.setText(loginViewModel.password.value)
 
         binding.username.editText?.onTextChanged { username -> loginViewModel.username.value = username }
         binding.password.editText?.onTextChanged { password -> loginViewModel.password.value = password }
+        binding.outlinedExposedDropdown.onItemClickListener =
+            AdapterView.OnItemClickListener { parent, view, position, id ->
+                val type = parent?.getItemAtPosition(position).toString()
+                loginViewModel.userType.value = type
+                loginViewModel.userId.value = "1"
+                Timber.d("userType: $type")
+            }
         binding.login.setOnClickListener {
             loginViewModel.onLoginClicked()
         }
 //        buttonRegister.onClick { viewModel.onRegisterClicked() }
 
     }
-
-    override fun onNothingSelected(parent: AdapterView<*>?) {
-        //Nothing for now
-    }
-
-    override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
-        val type = parent?.getItemAtPosition(position).toString()
-        loginViewModel.userType.value = type
-        loginViewModel.userId.value = "1"
-        Timber.d("userType: $type")
-    }
-
 }
